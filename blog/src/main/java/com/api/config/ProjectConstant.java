c7fb0ab034b568b75fb163d35b8e4526fe0de3f9
wanghuiwen <1>
2019-10-29 17:46:27 +0800

    init blog model

--- blog/src/main/java/com/api/config/ProjectConstant.java
+++ blog/src/main/java/com/api/config/ProjectConstant.java
@@ -1,8 +1,6 @@
 package com.api.config;
 
 
-import com.api.base.model.Role;
-
 /**
  * 项目常量
  */
@@ -14,8 +12,5 @@ public final class ProjectConstant {
     public static final String SERVICE_PACKAGE = BASE_PACKAGE + ".service";//Service所在包
     public static final String SERVICE_IMPL_PACKAGE = SERVICE_PACKAGE + ".impl";//ServiceImpl所在包
     public static final String CONTROLLER_PACKAGE = BASE_PACKAGE + ".web";//Controller所在包
-
     public static final String MAPPER_INTERFACE_REFERENCE = "com.api.core.Mapper";//Mapper插件基础接口的完全限定名
-    public static final String ROLE_ADMIN="admin";
-    public static final Role[] initRole={new Role("管理员",ROLE_ADMIN)};
 }
